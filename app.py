86b85711ea5a6c7f982e148fc7ba42f6ec850329
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-10-30 20:08:58 -0300

    first python script

---
+++ app.py
@@ -0,0 +1,23 @@
+print("Hello World...")
+
+# Esto es un comentario en Python // Comentarios en JavaScript
+# Esto es un comentario en Python /* comentarios en JavaScript */
+
+# Como definimos una variable en JS?
+# var myVariable = 235
+
+# En python en cambio es
+myVariable = 235
+myCadenaTexto = "Esto es una cadena de texto"
+mySuperVariable = None
+
+# Definimos una funcion que calcule el area de un rectangulo.
+# es el equivalente a escribir function en javaScript
+def calcularArea( base, altura ):
+    areaTotal = None
+    areaTotal = base * altura
+    return areaTotal
+
+resultado = calcularArea(10, 35)
+print(resultado)
+
